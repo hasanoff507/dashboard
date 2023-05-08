b8137fe569e717ac726a8c1c4cfdb74cd77cf5af
hasanoff507@gmail.com <hasanoff507@gmail.com>
2023-05-08 17:33:46 +0500

    asd

---
+++ src/Api/simple api/index.js
@@ -0,0 +1,20 @@
+export const mockData = [
+    {
+      id: 1,
+      title: 'USERS',
+     link:'/seller_accounts',
+      allPage: 'See all users'
+    },
+    {
+      id: 2,
+      title: 'PRODUCTS',
+      link: "/goods",
+      allPage: 'See all products'
+    },
+    {
+      id: 3,
+      title: 'CROLLER',
+      link: "/croller_goods",
+      allPage: 'See all croller'
+    }
+  ];
--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,10 +1,17 @@
 import "./widget.scss";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
-import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
-
+// import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+// import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+// import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+// import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+// import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import PersonIcon from '@mui/icons-material/Person';
+import { mockData } from "../../Api/simple api";
 const Widget = ({ type }) => {
   let data;
 
@@ -12,89 +19,110 @@ const Widget = ({ type }) => {
   const amount = 100;
   const diff = 20;
 
-  switch (type) {
-    case "user":
-      data = {
-        title: "USERS",
-        isMoney: false,
-        link: "See all users",
-        icon: (
-          <PersonOutlinedIcon
-            className="icon"
-            style={{
-              color: "crimson",
-              backgroundColor: "rgba(255, 0, 0, 0.2)",
-            }}
-          />
-        ),
-      };
-      break;
-    case "order":
-      data = {
-        title: "ORDERS",
-        isMoney: false,
-        link: "View all orders",
-        icon: (
-          <ShoppingCartOutlinedIcon
-            className="icon"
-            style={{
-              backgroundColor: "rgba(218, 165, 32, 0.2)",
-              color: "goldenrod",
-            }}
-          />
-        ),
-      };
-      break;
-    case "earning":
-      data = {
-        title: "EARNINGS",
-        isMoney: true,
-        link: "View net earnings",
-        icon: (
-          <MonetizationOnOutlinedIcon
-            className="icon"
-            style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
-          />
-        ),
-      };
-      break;
-    case "balance":
-      data = {
-        title: "BALANCE",
-        isMoney: true,
-        link: "See details",
-        icon: (
-          <AccountBalanceWalletOutlinedIcon
-            className="icon"
-            style={{
-              backgroundColor: "rgba(128, 0, 128, 0.2)",
-              color: "purple",
-            }}
-          />
-        ),
-      };
-      break;
-    default:
-      break;
-  }
+  // switch (type) {
+  //   case "user":
+  //     data = {
+  //       title: "USERS",
+  //       isMoney: false,
+  //       link: "See all users",
+  //       icon: (
+  //         <PersonOutlinedIcon
+  //           className="icon"
+  //           style={{
+  //             color: "crimson",
+  //             backgroundColor: "rgba(255, 0, 0, 0.2)",
+  //           }}
+  //         />
+  //       ),
+  //     };
+  //     break;
+  //   case "order":
+  //     data = {
+  //       title: "ORDERS",
+  //       isMoney: false,
+  //       link: "View all orders",
+  //       icon: (
+  //         <ShoppingCartOutlinedIcon
+  //           className="icon"
+  //           style={{
+  //             backgroundColor: "rgba(218, 165, 32, 0.2)",
+  //             color: "goldenrod",
+  //           }}
+  //         />
+  //       ),
+  //     };
+  //     break;
+  //   case "earning":
+  //     data = {
+  //       title: "EARNINGS",
+  //       isMoney: true,
+  //       link: "View net earnings",
+  //       icon: (
+  //         <MonetizationOnOutlinedIcon
+  //           className="icon"
+  //           style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
+  //         />
+  //       ),
+  //     };
+  //     break;
+  //   case "balance":
+  //     data = {
+  //       title: "BALANCE",
+  //       isMoney: true,
+  //       link: "See details",
+  //       icon: (
+  //         <AccountBalanceWalletOutlinedIcon
+  //           className="icon"
+  //           style={{
+  //             backgroundColor: "rgba(128, 0, 128, 0.2)",
+  //             color: "purple",
+  //           }}
+  //         />
+  //       ),
+  //     };
+  //     break;
+  //   default:
+  //     break;
+  // }
 
   return (
-    <div className="widget">
-      <div className="left">
-        <span className="title">{data.title}</span>
-        <span className="counter">
-          {data.isMoney && "$"} {amount}
-        </span>
-        <span className="link">{data.link}</span>
-      </div>
-      <div className="right">
-        <div className="percentage positive">
-          <KeyboardArrowUpIcon />
-          {diff} %
-        </div>
-        {data.icon}
-      </div>
+    // <div className="widget">
+    //   <div className="left">
+    //     <span className="title">{data.title}</span>
+    //     <span className="counter">
+    //       {data.isMoney && "$"} {amount}
+    //     </span>
+    //     <span className="link">{data.link}</span>
+    //   </div>
+    //   <div className="right">
+    //     <div className="percentage positive">
+    //       <KeyboardArrowUpIcon />
+    //       {diff} %
+    //     </div>
+    //     {data.icon}
+    //   </div>
+    // </div>
+    <div>
+      {mockData.map((item) => {
+        return (
+          <Card sx={{ maxWidth: 345 }} key={item.id}>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {item.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">See all users</Button>
+              <Button size="small"><PersonIcon/></Button>
+            </CardActions>
+          </Card>
+        )
+      })}
     </div>
+
   );
 };
 
